e1cc17b59b3a42e9b3b166fad58fc1d72119e3fe
franmgc <yo.fran.castro@gmail.com>
2025-03-12 00:13:34 +0100

    problema: flip coeff

--- AIE/FIR_BUFFER_1/aie_kernels/fir.cc
+++ AIE/FIR_BUFFER_1/aie_kernels/fir.cc
@@ -12,33 +12,35 @@ alignas(aie::vector_decl_align) int32 b[8] ={1024, 512, 256, 128, 64, 32, 16, 8}
 
 using namespace adf;
 
-// los buffers de entrada y salida se pasan por referencia para evitar tener que crear una copia local del buffer
-void fir_filter(input_buffer<int32,extents<NUM_SAM>,margin<MARGIN>> & in, output_buffer<int32,extents<NUM_SAM>> & out) {
-	auto inIter=aie::begin_vector<8>(in);
-	auto outIter=aie::begin_vector<8>(out);
 
-	aie::vector<int32,8> coeffs = aie::load_v<8>(b); // Load the coeffs vector with weights
+void fir_filter(input_buffer<int32,extents<NUM_SAM>,margin<MARGIN>> & in, output_buffer<int32,extents<NUM_SAM>> & out) {
+	auto inIter=aie::begin_vector<NF>(in);
+	auto outIter=aie::begin_vector<NF>(out);
+
+	aie::vector<int32,NF> coeffs = aie::load_v<NF>(b); // carga el array de coefs en un vector
 
 	aie::vector<int32,16> data;
-	aie::accum<acc80,8> acc;
-	aie::vector<int32,8> dataout;
+	aie::accum<acc80,NF> acc;
+	aie::vector<int32,NF> dataout;
 
-	data.insert(0,*inIter++); 	// Reading samples for - 0
+	data.insert(0,*inIter++); 	// Lee muestras - 0
 
 	for(unsigned i=0; i<NUM_SAM/16; i++) {
 
-		data.insert(0, *inIter++);	// Reading samples for - 0
-		acc = aie::sliding_mul<8,8>(coeffs, 0, data, 9);
-		// Convert acc to vector & Write it to out window and increment the pointer
+		data.insert(0, *inIter++);	// Lee muestras - 0
+
+        // hace MAC con valor inicial del acumulador = 0
+        // aie::accum<acc64, 8> sliding_mul<8U, 8U, 1, 1, 1, accauto, aie::vector<int, 8>, aie::vector<int, 16>>(const aie::vector<int, 8> &coeff, unsigned int coeff_start, const aie::vector<int, 16> &data, unsigned int data_start)
+		acc = aie::sliding_mul<NF,NF>(coeffs, 0, data, NF+1);
+
+        // convierte el acumulador a vector        
 		dataout.insert(0,acc.to_vector<int32>());
 		*outIter++=dataout;
 
+        // ---------
 
-		data.insert(1, *inIter++);	// Reading samples for - 1
-		// mul preforms multiplication accumulations with no initial value for the accumulator
-        // sliding_mul<4U, 8U, 1, 1, 1, accauto, aie::vector<int, 8>, aie::vector<int, 8>>
-		acc = aie::sliding_mul<8,8>(coeffs, 0, data, 1);
-		// Convert acc to vector & Write it to out window and increment the pointer
+		data.insert(1, *inIter++);	// Lee muestras - 1
+		acc = aie::sliding_mul<NF,NF>(coeffs, 0, data, 1);
 		dataout.insert(0,acc.to_vector<int32>());
 		*outIter++=dataout;
 
